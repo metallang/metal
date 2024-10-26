3e6e64ef83b15a07b47421fa4904cbd7e5bc825e
VincentRPS <vincentbusiness55@gmail.com>
2024-10-26 22:13:34 +0800

    feat: non-working uncompilable code
    Still need to rework to the unsafe LLVM sys (maybe)

--- crates/metal-ast/src/lib.rs
+++ crates/metal-ast/src/lib.rs
@@ -1,6 +1,5 @@
 //! The most honest-to-God representation of any piece of Metal code.
 
-mod codegen;
 mod expr;
 mod ty;
 
@@ -16,7 +15,7 @@ pub type Path<'src> = Vec<PathSegment<'src>>;
 
 /// An identifier.
 #[spanned]
-#[derive(Spanned)]
+#[derive(Spanned, Eq, PartialEq)]
 pub struct Ident<'src> {
     pub inner: &'src str,
 }
@@ -62,6 +61,11 @@ pub struct FnDefStmt<'src> {
     pub inputs: Vec<FnInput<'src>>,
     /// See [Ty].
     pub return_type: Option<Ty<'src>>,
+    /// Whether or not this function can be
+    /// externally accessed. Like, 'pub main() {}'
+    pub public: bool,
+    /// Where this function definition is located.
+    pub location: Path<'src>,
     /// See [Block].
     pub body: Block<'src>,
 }
