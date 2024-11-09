dcacaeb2a653cfee75b4bb49104eb27fa268b788
lena <77104725+elenakrittik@users.noreply.github.com>
2024-11-09 16:03:53 +0300

    feat(metal-parser): Implement the parser. (#25)

--- crates/metal-lexer/src/span.rs
+++ crates/metal-lexer/src/span.rs
@@ -25,3 +25,16 @@ impl<T: Spanned> Spanned for Vec<T> {
         span
     }
 }
+
+pub trait MaybeSpanned {
+    fn maybe_span(&self) -> Option<&Span>;
+}
+
+impl<T: Spanned> MaybeSpanned for Option<T> {
+    fn maybe_span(&self) -> Option<&Span> {
+        match self {
+            Some(spanned) => Some(spanned.span()),
+            None => None,
+        }
+    }
+}
---
+++ crates/metal-parser/src/block.rs
@@ -0,0 +1,26 @@
+use metal_ast::{Block, Item};
+use metal_lexer::{span, TokenKind};
+
+use crate::{expect, parse_delimited, parse_item, parser_type, Delimiter};
+
+pub fn parse_block<'src>(parser: parser_type!('src)) -> Block<'src> {
+    let start_span = expect!(parser, TokenKind::OpeningBrace);
+
+    let items = parse_block_raw(parser);
+
+    let end_span = expect!(parser, TokenKind::ClosingBrace);
+
+    Block {
+        items,
+        span: span!(start_span.start..end_span.end),
+    }
+}
+
+pub fn parse_block_raw<'src>(parser: parser_type!('src)) -> Vec<Item<'src>> {
+    parse_delimited(
+        parser,
+        Delimiter::Semicolon,
+        [Delimiter::ClosingBrace],
+        &mut parse_item,
+    )
+}
