80030fc1eb3d1c2604f0ea246c0cf4ff0c10b991
VincentRPS <vincentbusiness55@gmail.com>
2024-11-27 20:44:41 +0800

    feat(llvm): add function for compiling modules (#75)

--- crates/metal-codegen-llvm/src/lib.rs
+++ crates/metal-codegen-llvm/src/lib.rs
@@ -2,19 +2,25 @@
 
 //! Metal library for compiling to LLVM IR using MIR.
 
-use std::collections::HashMap;
+use std::{collections::HashMap, ffi::CString};
 
 use metal_mir::{
     parcel::Module,
     types::{visibility::Visibility, Type},
 };
 
+pub mod core;
 pub mod expr;
 pub mod primitives;
+pub mod safeties;
 pub mod stmt;
 pub mod ty;
 
 use llvm_sys::{
+    core::{
+        LLVMContextCreate, LLVMContextDispose, LLVMCreateBuilder, LLVMDisposeBuilder,
+        LLVMDisposeModule, LLVMModuleCreateWithNameInContext,
+    },
     prelude::{LLVMBuilderRef, LLVMContextRef, LLVMModuleRef, LLVMTypeRef, LLVMValueRef},
     LLVMLinkage,
 };
@@ -24,6 +30,31 @@ pub struct LLVMRefs {
     builder: LLVMBuilderRef,
     module: LLVMModuleRef,
     locals: HashMap<&'static str, LLVMValueRef>,
+}
+
+impl LLVMRefs {
+    pub fn new(module: &Module) -> Self {
+        let ctx = unsafe { LLVMContextCreate() };
+        let module_name = CString::new(module.name.as_str()).unwrap();
+        unsafe {
+            Self {
+                ctx,
+                builder: LLVMCreateBuilder(),
+                module: LLVMModuleCreateWithNameInContext(module_name.as_ptr(), ctx),
+                locals: HashMap::new(),
+            }
+        }
+    }
+}
+
+impl Drop for LLVMRefs {
+    fn drop(&mut self) {
+        unsafe {
+            LLVMDisposeBuilder(self.builder);
+            LLVMDisposeModule(self.module);
+            LLVMContextDispose(self.ctx);
+        }
+    }
 }
 
 pub trait CodeGenValue {
