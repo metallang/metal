58a46ef25b63e5189c2a484832c6e2316834da0e
VincentRPS <vincentbusiness55@gmail.com>
2024-12-02 22:27:53 +0800

    initial: some initial setup for drill

---
+++ crates/drill/src/main.rs
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+use debug_to_display::ForwardDebugToDisplay;
+use tapcli::Command;
+
+use crate::cli::Cli;
+use crate::error::Error;
+
+pub mod cli;
+pub mod error;
+
+fn main() -> Result<(), ForwardDebugToDisplay<Error>> {
+    let cli = Cli::from_env()?;
+
+    cli.run()?;
+
+    Ok(())
+}
