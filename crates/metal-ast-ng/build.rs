95a61756040f786160e40177cd8e5b52300eedbd
elenakrittik <dev.elenakrittik@gmail.com>
2024-12-03 16:45:26 +0300

    hello vic

--- crates/metal-ast-ng/build.rs
+++ crates/metal-ast-ng/build.rs
@@ -3,10 +3,10 @@ use std::process::Command;
 fn main() -> Result<(), std::io::Error> {
     println!("cargo::rerun-if-changed=./metal.ungram");
 
-    Command::new("cargo")
-        .args(["run", "-p", "scripts", "--bin", "ungram"])
-        .spawn()?
-        .wait()?;
+    // Command::new("cargo")
+    //     .args(["run", "-p", "scripts", "--bin", "ungram"])
+    //     .spawn()?
+    //     .wait()?;
 
     Ok(())
 }
--- scripts/ungram/names.rs
+++ scripts/ungram/names.rs
@@ -1,77 +1,76 @@
-struct Name<'a> {
-    inner: Cow<'a, String>,
-    generation: u8,
+use heck::{ToShoutySnakeCase, ToUpperCamelCase};
+
+pub struct IdentableGrammarName(String);
+
+fn syntax_kind_name(s: &str) -> IdentableGrammarName {
+    IdentableGrammarName(s.to_shouty_snake_case())
 }
 
-// fn syntax_kind_name(s: FormattedGrammarName<'_>) -> IdentableGrammarName {
-//     IdentableGrammarName(s.0.to_shouty_snake_case())
-// }
+fn ast_item_name(s: &str) -> IdentableGrammarName {
+    IdentableGrammarName(s.to_upper_camel_case())
+}
 
-// fn ast_item_name(s: FormattedGrammarName<'_>) -> IdentableGrammarName {
-//     IdentableGrammarName(s.0.to_upper_camel_case())
-// }
+fn token_fn_item_name(s: &str) -> IdentableGrammarName {
+    IdentableGrammarName(format!("{}_token", s))
+}
 
-// fn token_fn_item_name(s: FormattedGrammarName<'_>) -> IdentableGrammarName {
-//     IdentableGrammarName(format!("{}_token", s.0))
-// }
+fn node_visual_name(n: &str) -> &str {
+    match n {
+        "Visibility" => "Vis",
+        "Mutability" => "Mutness",
+        "ExprSpecifier" => "ExprSpec",
+        "TypeQualifier" => "TypeQual",
+        other => other,
+    }
+}
 
-// fn node_visual_name(n: RawGrammarName<'_>) -> FormattedGrammarName<'_> {
-//     FormattedGrammarName(match n {
-//         RawGrammarName("Visibility") => "Vis",
-//         RawGrammarName("Mutability") => "Mutness",
-//         RawGrammarName("ExprSpecifier") => "ExprSpec",
-//         RawGrammarName("TypeQualifier") => "TypeQual",
-//         other => other.0,
-//     })
-// }
-
-// fn token_visual_name(t: RawGrammarName<'_>) -> FormattedGrammarName<'_> {
-//     FormattedGrammarName(match t {
-//         RawGrammarName("@ident") => "lit_ident",
-//         RawGrammarName("@number") => "lit_num",
-//         RawGrammarName("@string") => "lit_str",
-//         RawGrammarName("=") => "eq",
-//         RawGrammarName(":") => "colon",
-//         RawGrammarName("{") => "l_brace",
-//         RawGrammarName("}") => "r_brace",
-//         RawGrammarName(",") => "comma",
-//         RawGrammarName("(") => "l_paren",
-//         RawGrammarName(")") => "r_paren",
-//         RawGrammarName(";") => "semicolon",
-//         RawGrammarName(".") => "dot",
-//         RawGrammarName("+") => "plus",
-//         RawGrammarName("-") => "minus",
-//         RawGrammarName("!") => "bang",
-//         RawGrammarName("~") => "tilde",
-//         RawGrammarName("+=") => "plus_eq",
-//         RawGrammarName("-=") => "minus_eq",
-//         RawGrammarName("/=") => "slash_eq",
-//         RawGrammarName("*=") => "star_eq",
-//         RawGrammarName("**=") => "star2_eq",
-//         RawGrammarName("%=") => "percent_eq",
-//         RawGrammarName("^=") => "caret_eq",
-//         RawGrammarName("&=") => "amp_eq",
-//         RawGrammarName("|=") => "pipe_eq",
-//         RawGrammarName("<<=") => "shiftl_eq",
-//         RawGrammarName(">>=") => "shiftr_eq",
-//         RawGrammarName("/") => "slash",
-//         RawGrammarName("*") => "star",
-//         RawGrammarName("**") => "star2",
-//         RawGrammarName("%") => "percent",
-//         RawGrammarName("&&") => "amp2",
-//         RawGrammarName("||") => "pipe2",
-//         RawGrammarName("==") => "eq2",
-//         RawGrammarName("!=") => "bang_eq",
-//         RawGrammarName(">") => "gt",
-//         RawGrammarName(">=") => "gt_eq",
-//         RawGrammarName("<") => "lt",
-//         RawGrammarName("<=") => "lt_eq",
-//         RawGrammarName("^") => "caret",
-//         RawGrammarName("&") => "amp",
-//         RawGrammarName("|") => "pipe",
-//         RawGrammarName("<<") => "lt2",
-//         RawGrammarName(">>") => "gt2",
-//         RawGrammarName("..") => "dot2",
-//         other => other.0,
-//     })
-// }
+fn token_visual_name(t: &str) -> &str {
+    match t {
+        "@ident" => "lit_ident",
+        "@number" => "lit_num",
+        "@string" => "lit_str",
+        "=" => "eq",
+        ":" => "colon",
+        "{" => "l_brace",
+        "}" => "r_brace",
+        "," => "comma",
+        "(" => "l_paren",
+        ")" => "r_paren",
+        ";" => "semicolon",
+        "." => "dot",
+        "+" => "plus",
+        "-" => "minus",
+        "!" => "bang",
+        "~" => "tilde",
+        "+=" => "plus_eq",
+        "-=" => "minus_eq",
+        "/=" => "slash_eq",
+        "*=" => "star_eq",
+        "**=" => "star2_eq",
+        "%=" => "percent_eq",
+        "^=" => "caret_eq",
+        "&=" => "amp_eq",
+        "|=" => "pipe_eq",
+        "<<=" => "shiftl_eq",
+        ">>=" => "shiftr_eq",
+        "/" => "slash",
+        "*" => "star",
+        "**" => "star2",
+        "%" => "percent",
+        "&&" => "amp2",
+        "||" => "pipe2",
+        "==" => "eq2",
+        "!=" => "bang_eq",
+        ">" => "gt",
+        ">=" => "gt_eq",
+        "<" => "lt",
+        "<=" => "lt_eq",
+        "^" => "caret",
+        "&" => "amp",
+        "|" => "pipe",
+        "<<" => "lt2",
+        ">>" => "gt2",
+        ".." => "dot2",
+        other => other,
+    }
+}
