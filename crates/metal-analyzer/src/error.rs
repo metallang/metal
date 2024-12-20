f31ca1b6b0eaf14adbafea5767d4facf110b57a8
VincentRPS <vincentbusiness55@gmail.com>
2024-12-20 21:19:51 +0800

    Apply suggestions from code review
    Co-authored-by: lena <77104725+elenakrittik@users.noreply.github.com>

--- crates/metal-analyzer/src/error.rs
+++ crates/metal-analyzer/src/error.rs
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-use std::io;
 
 #[derive(Debug, thiserror::Error)]
 pub enum AnalyzerError {
     #[error("Error with file IO system: {0}")]
-    IOError(#[from] io::Error),
+    IOError(#[from] std::io::Error),
     #[error("Bincode error: {0}")]
     RkyvError(#[from] bincode::Error),
 }
