092aacd3bf27d1b47bc53ee94c40465a411f085f
lena <77104725+elenakrittik@users.noreply.github.com>
2025-03-18 16:59:44 +0300

    refactor: Introduce `Stmt` (#123)

--- crates/metal-parser-ng/src/block.rs
+++ crates/metal-parser-ng/src/block.rs
@@ -3,24 +3,24 @@
 use metal_ast_ng::N;
 use metal_ast_ng::T;
 
-use crate::item::parse_item;
 use crate::parser::parser_type;
+use crate::stmt::parse_stmt;
 
 pub fn parse_block(parser: &mut parser_type!()) {
     parser.start_node(N![Block]);
 
     parser.maybe_eat(T!['{']);
-    parse_block_items(parser);
+    parse_block_stmts(parser);
     parser.maybe_eat(T!['}']);
 
     parser.end_node();
 }
 
-pub fn parse_block_items(parser: &mut parser_type!()) {
-    parser.start_node(N![BlockItems]);
+pub fn parse_block_stmts(parser: &mut parser_type!()) {
+    parser.start_node(N![BlockStmts]);
 
     while !(parser.peek_is(T!['}']) || parser.is_eof()) {
-        parse_item(parser);
+        parse_stmt(parser);
     }
 
     parser.end_node();
