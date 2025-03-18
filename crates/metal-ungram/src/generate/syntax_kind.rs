e1a42f0e3a0a4f9484937630de247fb8157bf308
lena <77104725+elenakrittik@users.noreply.github.com>
2025-03-18 16:45:40 +0300

    fix: Make `generate_t` use `PAREN_TOKENS` for quote-wrap-needed detection (#127)

--- crates/metal-ungram/src/generate/syntax_kind.rs
+++ crates/metal-ungram/src/generate/syntax_kind.rs
@@ -4,7 +4,7 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 use crate::engram::Engram;
-use crate::grammar_item::{GrammarItem, GrammarItemInfo};
+use crate::grammar_item::{GrammarItem, GrammarItemInfo, PAREN_TOKENS};
 
 /// Generates the `syntax_kind.rs` file.
 pub fn generate_syntax_kind_file(grammar: &Engram) -> TokenStream {
@@ -91,17 +91,17 @@ fn generate_syntax_kind(grammar: &Engram) -> TokenStream {
 /// Generates the `T!` macro.
 fn generate_t_macro(grammar: &Engram) -> TokenStream {
     let arms = grammar.tokens().map(|token| {
-        let token_name = token.name.as_str().parse().unwrap_or_else(|_| {
+        let token_name = token.name.as_str();
+        let token_name = if PAREN_TOKENS.contains(&token_name) {
             // rust requires braces to be always paired regardless of where they appear,
             // so we can't make e.g. `T![}]` work. instead, we'll wrap such tokens in quotes
-            let token_name = token.name.as_str();
-
-            assert_eq!(token_name.len(), 1);
 
             let token_name = token_name.chars().next().unwrap();
 
             quote! { #token_name }
-        });
+        } else {
+            token_name.parse().unwrap()
+        };
 
         let syntax_kind_name = token.syntax_kind_info().ident;
 
