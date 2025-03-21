600db132d5b10236b3ca810c2717921a9bd2e466
elenakrittik <dev.elenakrittik@gmail.com>
2025-03-21 22:03:38 +0300

    hi

--- crates/metal-ast/src/syntax_kind.rs
+++ crates/metal-ast/src/syntax_kind.rs
@@ -330,46 +330,70 @@ impl SyntaxKind {
 /// # }
 /// ```
 pub macro T {
-    ['{'] => { $crate ::SyntaxKind::L_BRACE_TOKEN }, ['}'] => { $crate
-    ::SyntaxKind::R_BRACE_TOKEN }, [@ ident] => { $crate ::SyntaxKind::LIT_IDENT_TOKEN },
-    [pub] => { $crate ::SyntaxKind::PUB_TOKEN }, [mut] => { $crate
-    ::SyntaxKind::MUT_TOKEN }, [=] => { $crate ::SyntaxKind::EQ_TOKEN }, [:] => { $crate
-    ::SyntaxKind::COLON_TOKEN }, [;] => { $crate ::SyntaxKind::SEMICOLON_TOKEN }, [@] =>
-    { $crate ::SyntaxKind::AT_TOKEN }, [abstract] => { $crate
-    ::SyntaxKind::ABSTRACT_TOKEN }, [def] => { $crate ::SyntaxKind::DEF_TOKEN }, [const]
-    => { $crate ::SyntaxKind::CONST_TOKEN }, [enum] => { $crate ::SyntaxKind::ENUM_TOKEN
-    }, ['('] => { $crate ::SyntaxKind::L_PAREN_TOKEN }, [')'] => { $crate
-    ::SyntaxKind::R_PAREN_TOKEN }, [,] => { $crate ::SyntaxKind::COMMA_TOKEN }, [import]
-    => { $crate ::SyntaxKind::IMPORT_TOKEN }, [.] => { $crate ::SyntaxKind::DOT_TOKEN },
-    [struct] => { $crate ::SyntaxKind::STRUCT_TOKEN }, [type] => { $crate
-    ::SyntaxKind::TYPE_TOKEN }, ['['] => { $crate ::SyntaxKind::L_BRACKET_TOKEN }, [']']
-    => { $crate ::SyntaxKind::R_BRACKET_TOKEN }, [&] => { $crate ::SyntaxKind::AMP_TOKEN
-    }, [+] => { $crate ::SyntaxKind::PLUS_TOKEN }, [-] => { $crate
-    ::SyntaxKind::MINUS_TOKEN }, [!] => { $crate ::SyntaxKind::BANG_TOKEN }, [~] => {
-    $crate ::SyntaxKind::TILDE_TOKEN }, [*] => { $crate ::SyntaxKind::STAR_TOKEN }, [+=]
-    => { $crate ::SyntaxKind::PLUS_EQ_TOKEN }, [-=] => { $crate
-    ::SyntaxKind::MINUS_EQ_TOKEN }, [/=] => { $crate ::SyntaxKind::SLASH_EQ_TOKEN }, [*=]
-    => { $crate ::SyntaxKind::STAR_EQ_TOKEN }, [**=] => { $crate
-    ::SyntaxKind::STAR2_EQ_TOKEN }, [%=] => { $crate ::SyntaxKind::PERCENT_EQ_TOKEN },
-    [^=] => { $crate ::SyntaxKind::CARET_EQ_TOKEN }, [&=] => { $crate
-    ::SyntaxKind::AMP_EQ_TOKEN }, [|=] => { $crate ::SyntaxKind::PIPE_EQ_TOKEN }, [<<=]
-    => { $crate ::SyntaxKind::LT2_EQ_TOKEN }, [>>=] => { $crate
-    ::SyntaxKind::GT2_EQ_TOKEN }, [/] => { $crate ::SyntaxKind::SLASH_TOKEN }, [**] => {
-    $crate ::SyntaxKind::STAR2_TOKEN }, [%] => { $crate ::SyntaxKind::PERCENT_TOKEN },
-    [&&] => { $crate ::SyntaxKind::AMP2_TOKEN }, [||] => { $crate
-    ::SyntaxKind::PIPE2_TOKEN }, [==] => { $crate ::SyntaxKind::EQ2_TOKEN }, [!=] => {
-    $crate ::SyntaxKind::BANG_EQ_TOKEN }, [>] => { $crate ::SyntaxKind::GT_TOKEN }, [>=]
-    => { $crate ::SyntaxKind::GT_EQ_TOKEN }, [<] => { $crate ::SyntaxKind::LT_TOKEN },
-    [<=] => { $crate ::SyntaxKind::LT_EQ_TOKEN }, [^] => { $crate
-    ::SyntaxKind::CARET_TOKEN }, [|] => { $crate ::SyntaxKind::PIPE_TOKEN }, [<<] => {
-    $crate ::SyntaxKind::LT2_TOKEN }, [>>] => { $crate ::SyntaxKind::GT2_TOKEN }, [..] =>
-    { $crate ::SyntaxKind::DOT2_TOKEN }, [@ number] => { $crate
-    ::SyntaxKind::LIT_NUM_TOKEN }, [@ string] => { $crate ::SyntaxKind::LIT_STR_TOKEN },
-    [return] => { $crate ::SyntaxKind::RETURN_TOKEN }, [if] => { $crate
-    ::SyntaxKind::IF_TOKEN }, [else] => { $crate ::SyntaxKind::ELSE_TOKEN }, [defer] => {
-    $crate ::SyntaxKind::DEFER_TOKEN }, [@ comment] => { $crate
-    ::SyntaxKind::COMMENT_TOKEN }, [@ whitespace] => { $crate
-    ::SyntaxKind::WHITESPACE_TOKEN }, [@ unknown] => { $crate ::SyntaxKind::UNKNOWN_TOKEN
+    ['{'] => { $crate::SyntaxKind::L_BRACE_TOKEN },
+    ['}'] => { $crate::SyntaxKind::R_BRACE_TOKEN },
+    [@ident] => { $crate::SyntaxKind::LIT_IDENT_TOKEN },
+    [pub] => { $crate::SyntaxKind::PUB_TOKEN },
+    [mut] => { $crate::SyntaxKind::MUT_TOKEN },
+    [=] => { $crate::SyntaxKind::EQ_TOKEN },
+    [:] => { $crate::SyntaxKind::COLON_TOKEN },
+    [;] => { $crate::SyntaxKind::SEMICOLON_TOKEN },
+    [@] => { $crate::SyntaxKind::AT_TOKEN },
+    [abstract] => { $crate::SyntaxKind::ABSTRACT_TOKEN },
+    [def] => { $crate::SyntaxKind::DEF_TOKEN },
+    [const] => { $crate::SyntaxKind::CONST_TOKEN },
+    [enum] => { $crate::SyntaxKind::ENUM_TOKEN },
+    ['('] => { $crate::SyntaxKind::L_PAREN_TOKEN },
+    [')'] => { $crate::SyntaxKind::R_PAREN_TOKEN },
+    [,] => { $crate::SyntaxKind::COMMA_TOKEN },
+    [import] => { $crate::SyntaxKind::IMPORT_TOKEN },
+    [.] => { $crate::SyntaxKind::DOT_TOKEN },
+    [struct] => { $crate::SyntaxKind::STRUCT_TOKEN },
+    [type] => { $crate::SyntaxKind::TYPE_TOKEN },
+    ['['] => { $crate::SyntaxKind::L_BRACKET_TOKEN },
+    [']'] => { $crate::SyntaxKind::R_BRACKET_TOKEN },
+    [&] => { $crate::SyntaxKind::AMP_TOKEN },
+    [+] => { $crate::SyntaxKind::PLUS_TOKEN },
+    [-] => { $crate::SyntaxKind::MINUS_TOKEN },
+    [!] => { $crate::SyntaxKind::BANG_TOKEN },
+    [~] => { $crate::SyntaxKind::TILDE_TOKEN },
+    [*] => { $crate::SyntaxKind::STAR_TOKEN },
+    [+=] => { $crate::SyntaxKind::PLUS_EQ_TOKEN },
+    [-=] => { $crate::SyntaxKind::MINUS_EQ_TOKEN },
+    [/=] => { $crate::SyntaxKind::SLASH_EQ_TOKEN },
+    [*=] => { $crate::SyntaxKind::STAR_EQ_TOKEN },
+    [**=] => { $crate::SyntaxKind::STAR2_EQ_TOKEN },
+    [%=] => { $crate::SyntaxKind::PERCENT_EQ_TOKEN },
+    [^=] => { $crate::SyntaxKind::CARET_EQ_TOKEN },
+    [&=] => { $crate::SyntaxKind::AMP_EQ_TOKEN },
+    [|=] => { $crate::SyntaxKind::PIPE_EQ_TOKEN },
+    [<<=] => { $crate::SyntaxKind::LT2_EQ_TOKEN },
+    [>>=] => { $crate::SyntaxKind::GT2_EQ_TOKEN },
+    [/] => { $crate::SyntaxKind::SLASH_TOKEN },
+    [**] => { $crate::SyntaxKind::STAR2_TOKEN },
+    [%] => { $crate::SyntaxKind::PERCENT_TOKEN },
+    [&&] => { $crate::SyntaxKind::AMP2_TOKEN },
+    [||] => { $crate::SyntaxKind::PIPE2_TOKEN },
+    [==] => { $crate::SyntaxKind::EQ2_TOKEN },
+    [!=] => { $crate::SyntaxKind::BANG_EQ_TOKEN },
+    [>] => { $crate::SyntaxKind::GT_TOKEN },
+    [>=] => { $crate::SyntaxKind::GT_EQ_TOKEN },
+    [<] => { $crate::SyntaxKind::LT_TOKEN },
+    [<=] => { $crate::SyntaxKind::LT_EQ_TOKEN },
+    [^] => { $crate::SyntaxKind::CARET_TOKEN },
+    [|] => { $crate::SyntaxKind::PIPE_TOKEN },
+    [<<] => { $crate::SyntaxKind::LT2_TOKEN },
+    [>>] => { $crate::SyntaxKind::GT2_TOKEN },
+    [..] => { $crate::SyntaxKind::DOT2_TOKEN },
+    [@number] => { $crate::SyntaxKind::LIT_NUM_TOKEN },
+    [@string] => { $crate::SyntaxKind::LIT_STR_TOKEN },
+    [return] => { $crate::SyntaxKind::RETURN_TOKEN },
+    [if] => { $crate::SyntaxKind::IF_TOKEN },
+    [else] => { $crate::SyntaxKind::ELSE_TOKEN },
+    [defer] => { $crate::SyntaxKind::DEFER_TOKEN },
+    [@comment] => { $crate::SyntaxKind::COMMENT_TOKEN },
+    [@whitespace] => { $crate::SyntaxKind::WHITESPACE_TOKEN },
+    [@unknown] => { $crate::SyntaxKind::UNKNOWN_TOKEN
     },
 }
 /// Returns the [SyntaxKind] variant corresponding to the provided node
@@ -384,59 +408,68 @@ pub macro T {
 /// # }
 /// ```
 pub macro N {
-    [Root] => { $crate ::SyntaxKind::ROOT_NODE }, [BlockStmts] => { $crate
-    ::SyntaxKind::BLOCK_STMTS_NODE }, [Block] => { $crate ::SyntaxKind::BLOCK_NODE },
-    [Stmt] => { $crate ::SyntaxKind::STMT_NODE }, [Name] => { $crate
-    ::SyntaxKind::NAME_NODE }, [Visibility] => { $crate ::SyntaxKind::VIS_NODE },
-    [Mutability] => { $crate ::SyntaxKind::MUTNESS_NODE }, [ExprSpecifier] => { $crate
-    ::SyntaxKind::EXPR_SPEC_NODE }, [Expr] => { $crate ::SyntaxKind::EXPR_NODE },
-    [TypeQualifier] => { $crate ::SyntaxKind::TYPE_QUAL_NODE }, [Type] => { $crate
-    ::SyntaxKind::TYPE_NODE }, [TypeSpecifier] => { $crate
-    ::SyntaxKind::TYPE_SPECIFIER_NODE }, [StmtKind] => { $crate
-    ::SyntaxKind::STMT_KIND_NODE }, [Item] => { $crate ::SyntaxKind::ITEM_NODE },
-    [Annotations] => { $crate ::SyntaxKind::ANNOTATIONS_NODE }, [ItemKind] => { $crate
-    ::SyntaxKind::ITEM_KIND_NODE }, [AbstractItem] => { $crate
-    ::SyntaxKind::ABSTRACT_ITEM_NODE }, [ConstItem] => { $crate
-    ::SyntaxKind::CONST_ITEM_NODE }, [EnumItem] => { $crate ::SyntaxKind::ENUM_ITEM_NODE
-    }, [FnItem] => { $crate ::SyntaxKind::FN_ITEM_NODE }, [ImportItem] => { $crate
-    ::SyntaxKind::IMPORT_ITEM_NODE }, [StructItem] => { $crate
-    ::SyntaxKind::STRUCT_ITEM_NODE }, [TypeAliasItem] => { $crate
-    ::SyntaxKind::TYPE_ALIAS_ITEM_NODE }, [Annotation] => { $crate
-    ::SyntaxKind::ANNOTATION_NODE }, [GenericParamList] => { $crate
-    ::SyntaxKind::GENERIC_PARAM_LIST_NODE }, [AbstractBody] => { $crate
-    ::SyntaxKind::ABSTRACT_BODY_NODE }, [AbstractFnItem] => { $crate
-    ::SyntaxKind::ABSTRACT_FN_ITEM_NODE }, [FnSignature] => { $crate
-    ::SyntaxKind::FN_SIGNATURE_NODE }, [EnumBody] => { $crate
-    ::SyntaxKind::ENUM_BODY_NODE }, [EnumBodyItem] => { $crate
-    ::SyntaxKind::ENUM_BODY_ITEM_NODE }, [EnumVariant] => { $crate
-    ::SyntaxKind::ENUM_VARIANT_NODE }, [EnumFn] => { $crate ::SyntaxKind::ENUM_FN_NODE },
-    [EnumVariantDataType] => { $crate ::SyntaxKind::ENUM_VARIANT_DATA_TYPE_NODE },
-    [FnInputs] => { $crate ::SyntaxKind::FN_INPUTS_NODE }, [FnInput] => { $crate
-    ::SyntaxKind::FN_INPUT_NODE }, [ImportTree] => { $crate
-    ::SyntaxKind::IMPORT_TREE_NODE }, [ImportLeaf] => { $crate
-    ::SyntaxKind::IMPORT_LEAF_NODE }, [ImportBranch] => { $crate
-    ::SyntaxKind::IMPORT_BRANCH_NODE }, [ImportSegment] => { $crate
-    ::SyntaxKind::IMPORT_SEGMENT_NODE }, [ImportBranchSubtrees] => { $crate
-    ::SyntaxKind::IMPORT_BRANCH_SUBTREES_NODE }, [StructBody] => { $crate
-    ::SyntaxKind::STRUCT_BODY_NODE }, [StructBodyItem] => { $crate
-    ::SyntaxKind::STRUCT_BODY_ITEM_NODE }, [StructField] => { $crate
-    ::SyntaxKind::STRUCT_FIELD_NODE }, [StructFn] => { $crate
-    ::SyntaxKind::STRUCT_FN_NODE }, [NameType] => { $crate ::SyntaxKind::NAME_TYPE_NODE
-    }, [RefType] => { $crate ::SyntaxKind::REF_TYPE_NODE }, [BinaryType] => { $crate
-    ::SyntaxKind::BINARY_TYPE_NODE }, [NameTypeGenerics] => { $crate
-    ::SyntaxKind::NAME_TYPE_GENERICS_NODE }, [NameTypeGenericsInner] => { $crate
-    ::SyntaxKind::NAME_TYPE_GENERICS_INNER_NODE }, [BinaryTypeOp] => { $crate
-    ::SyntaxKind::BINARY_TYPE_OP_NODE }, [PrefixExpr] => { $crate
-    ::SyntaxKind::PREFIX_EXPR_NODE }, [BinaryExpr] => { $crate
-    ::SyntaxKind::BINARY_EXPR_NODE }, [CallExpr] => { $crate ::SyntaxKind::CALL_EXPR_NODE
-    }, [LitExpr] => { $crate ::SyntaxKind::LIT_EXPR_NODE }, [ParenExpr] => { $crate
-    ::SyntaxKind::PAREN_EXPR_NODE }, [ReturnExpr] => { $crate
-    ::SyntaxKind::RETURN_EXPR_NODE }, [IfExpr] => { $crate ::SyntaxKind::IF_EXPR_NODE },
-    [DeferExpr] => { $crate ::SyntaxKind::DEFER_EXPR_NODE }, [PrefixExprOp] => { $crate
-    ::SyntaxKind::PREFIX_EXPR_OP_NODE }, [BinaryExprOp] => { $crate
-    ::SyntaxKind::BINARY_EXPR_OP_NODE }, [CallExprArgs] => { $crate
-    ::SyntaxKind::CALL_EXPR_ARGS_NODE }, [IfExprElseClause] => { $crate
-    ::SyntaxKind::IF_EXPR_ELSE_CLAUSE_NODE }, [GenericParams] => { $crate
-    ::SyntaxKind::GENERIC_PARAMS_NODE }, [GenericParam] => { $crate
-    ::SyntaxKind::GENERIC_PARAM_NODE },
+    [Root] => { $crate::SyntaxKind::ROOT_NODE },
+    [BlockStmts] => { $crate::SyntaxKind::BLOCK_STMTS_NODE },
+    [Block] => { $crate::SyntaxKind::BLOCK_NODE },
+    [Stmt] => { $crate::SyntaxKind::STMT_NODE },
+    [Name] => { $crate::SyntaxKind::NAME_NODE },
+    [Visibility] => { $crate::SyntaxKind::VIS_NODE },
+    [Mutability] => { $crate::SyntaxKind::MUTNESS_NODE },
+    [ExprSpecifier] => { $crate::SyntaxKind::EXPR_SPEC_NODE },
+    [Expr] => { $crate::SyntaxKind::EXPR_NODE },
+    [TypeQualifier] => { $crate::SyntaxKind::TYPE_QUAL_NODE },
+    [Type] => { $crate::SyntaxKind::TYPE_NODE },
+    [TypeSpecifier] => { $crate::SyntaxKind::TYPE_SPECIFIER_NODE },
+    [StmtKind] => { $crate::SyntaxKind::STMT_KIND_NODE },
+    [Item] => { $crate::SyntaxKind::ITEM_NODE },
+    [Annotations] => { $crate::SyntaxKind::ANNOTATIONS_NODE },
+    [ItemKind] => { $crate::SyntaxKind::ITEM_KIND_NODE },
+    [AbstractItem] => { $crate::SyntaxKind::ABSTRACT_ITEM_NODE },
+    [ConstItem] => { $crate::SyntaxKind::CONST_ITEM_NODE },
+    [EnumItem] => { $crate::SyntaxKind::ENUM_ITEM_NODE },
+    [FnItem] => { $crate::SyntaxKind::FN_ITEM_NODE },
+    [ImportItem] => { $crate::SyntaxKind::IMPORT_ITEM_NODE },
+    [StructItem] => { $crate::SyntaxKind::STRUCT_ITEM_NODE },
+    [TypeAliasItem] => { $crate::SyntaxKind::TYPE_ALIAS_ITEM_NODE },
+    [Annotation] => { $crate::SyntaxKind::ANNOTATION_NODE },
+    [GenericParamList] => { $crate::SyntaxKind::GENERIC_PARAM_LIST_NODE },
+    [AbstractBody] => { $crate::SyntaxKind::ABSTRACT_BODY_NODE },
+    [AbstractFnItem] => { $crate::SyntaxKind::ABSTRACT_FN_ITEM_NODE },
+    [FnSignature] => { $crate::SyntaxKind::FN_SIGNATURE_NODE },
+    [EnumBody] => { $crate::SyntaxKind::ENUM_BODY_NODE },
+    [EnumBodyItem] => { $crate::SyntaxKind::ENUM_BODY_ITEM_NODE },
+    [EnumVariant] => { $crate::SyntaxKind::ENUM_VARIANT_NODE },
+    [EnumFn] => { $crate::SyntaxKind::ENUM_FN_NODE },
+    [EnumVariantDataType] => { $crate::SyntaxKind::ENUM_VARIANT_DATA_TYPE_NODE },
+    [FnInputs] => { $crate::SyntaxKind::FN_INPUTS_NODE },
+    [FnInput] => { $crate::SyntaxKind::FN_INPUT_NODE },
+    [ImportTree] => { $crate::SyntaxKind::IMPORT_TREE_NODE },
+    [ImportLeaf] => { $crate::SyntaxKind::IMPORT_LEAF_NODE },
+    [ImportBranch] => { $crate::SyntaxKind::IMPORT_BRANCH_NODE },
+    [ImportSegment] => { $crate::SyntaxKind::IMPORT_SEGMENT_NODE },
+    [ImportBranchSubtrees] => { $crate::SyntaxKind::IMPORT_BRANCH_SUBTREES_NODE },
+    [StructBody] => { $crate::SyntaxKind::STRUCT_BODY_NODE },
+    [StructBodyItem] => { $crate::SyntaxKind::STRUCT_BODY_ITEM_NODE },
+    [StructField] => { $crate::SyntaxKind::STRUCT_FIELD_NODE },
+    [StructFn] => { $crate::SyntaxKind::STRUCT_FN_NODE },
+    [NameType] => { $crate::SyntaxKind::NAME_TYPE_NODE },
+    [RefType] => { $crate::SyntaxKind::REF_TYPE_NODE },
+    [BinaryType] => { $crate::SyntaxKind::BINARY_TYPE_NODE },
+    [NameTypeGenerics] => { $crate::SyntaxKind::NAME_TYPE_GENERICS_NODE },
+    [NameTypeGenericsInner] => { $crate::SyntaxKind::NAME_TYPE_GENERICS_INNER_NODE },
+    [BinaryTypeOp] => { $crate::SyntaxKind::BINARY_TYPE_OP_NODE },
+    [PrefixExpr] => { $crate::SyntaxKind::PREFIX_EXPR_NODE },
+    [BinaryExpr] => { $crate::SyntaxKind::BINARY_EXPR_NODE },
+    [CallExpr] => { $crate::SyntaxKind::CALL_EXPR_NODE },
+    [LitExpr] => { $crate::SyntaxKind::LIT_EXPR_NODE },
+    [ParenExpr] => { $crate::SyntaxKind::PAREN_EXPR_NODE },
+    [ReturnExpr] => { $crate::SyntaxKind::RETURN_EXPR_NODE },
+    [IfExpr] => { $crate::SyntaxKind::IF_EXPR_NODE },
+    [DeferExpr] => { $crate::SyntaxKind::DEFER_EXPR_NODE },
+    [PrefixExprOp] => { $crate::SyntaxKind::PREFIX_EXPR_OP_NODE },
+    [BinaryExprOp] => { $crate::SyntaxKind::BINARY_EXPR_OP_NODE },
+    [CallExprArgs] => { $crate::SyntaxKind::CALL_EXPR_ARGS_NODE },
+    [IfExprElseClause] => { $crate::SyntaxKind::IF_EXPR_ELSE_CLAUSE_NODE },
+    [GenericParams] => { $crate::SyntaxKind::GENERIC_PARAMS_NODE },
+    [GenericParam] => { $crate::SyntaxKind::GENERIC_PARAM_NODE },
 }
